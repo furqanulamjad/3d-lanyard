346cf1fee75ca96e4d5e2503861920b186892ac4
furqanulamjad <sirbloxy007@gmail.com>
2025-11-29 17:50:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
 
-useGLTF.preload('https://github.com/furqanulamjad/3d-lanyard/raw/refs/heads/main/card.glb')
+useGLTF.preload('https://github.com/furqanulamjad/3d-lanyard/raw/refs/heads/main/tag.glb')
 useTexture.preload('https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/SOT1hmCesOHxEYxL7vkoZ/c57b29c85912047c414311723320c16b/band.jpg')
 
 export default function App() {
@@ -36,7 +36,7 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
   const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef()
   const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3()
   const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 2, linearDamping: 2 }
-  const { nodes, materials } = useGLTF('https://github.com/furqanulamjad/3d-lanyard/raw/refs/heads/main/card.glb')
+  const { nodes, materials } = useGLTF('https://github.com/furqanulamjad/3d-lanyard/raw/refs/heads/main/tag.glb')
   const texture = useTexture('https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/SOT1hmCesOHxEYxL7vkoZ/c57b29c85912047c414311723320c16b/band.jpg')
   const { width, height } = useThree((state) => state.size)
   const [curve] = useState(() => new THREE.CatmullRomCurve3([new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()]))
