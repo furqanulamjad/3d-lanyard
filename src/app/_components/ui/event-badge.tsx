3dcc95467c245940b759aef9b020219f694e9ffc
furqanulamjad <sirbloxy007@gmail.com>
2025-11-29 19:15:04 +0800

    Update event-badge.tsx

--- src/app/_components/ui/event-badge.tsx
+++ src/app/_components/ui/event-badge.tsx
@@ -9,7 +9,7 @@ import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
 import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
-useGLTF.preload('https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/5huRVDzcoDwnbgrKUo1Lzs/53b6dd7d6b4ffcdbd338fa60265949e1/tag.glb')
+useGLTF.preload('/models/tag.glb')
 useTexture.preload('https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/SOT1hmCesOHxEYxL7vkoZ/c57b29c85912047c414311723320c16b/band.jpg')
 
 export default function App() {
@@ -121,4 +121,5 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
       </mesh>
     </>
   )
+
 }
