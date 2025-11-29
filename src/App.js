68026ff08e4cf037265ef27d8a4abb3abacd714b
furqanulamjad <sirbloxy007@gmail.com>
2025-11-29 19:10:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
 
-useGLTF.preload('/3d/tag.glb')
+useGLTF.preload('https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/5huRVDzcoDwnbgrKUo1Lzs/53b6dd7d6b4ffcdbd338fa60265949e1/tag.glb')
 useTexture.preload('https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/SOT1hmCesOHxEYxL7vkoZ/c57b29c85912047c414311723320c16b/band.jpg')
 
 export default function App() {
